81984440705f8befb9f51fa740e5fe80fdc5217f
unknown <hyunjiseung0x@gmail.com>
2023-11-21 14:36:56 +0900

    START

---
+++ TypingGameDlg.cpp
@@ -0,0 +1,207 @@
+﻿
+// TypingGameDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "TypingGame.h"
+#include "TypingGameDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CTypingGameDlg 대화 상자
+
+
+
+CTypingGameDlg::CTypingGameDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_TYPINGGAME_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CTypingGameDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_TAB, m_tab);
+}
+
+BEGIN_MESSAGE_MAP(CTypingGameDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_NOTIFY(TCN_SELCHANGE, IDC_TAB, &CTypingGameDlg::OnSelchangeTab)
+END_MESSAGE_MAP()
+
+
+// CTypingGameDlg 메시지 처리기
+
+BOOL CTypingGameDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+	SetWindowPos(NULL, 0, 0, 1400, 1000, SWP_NOMOVE | SWP_NOZORDER);
+	CRect rect;
+	m_tab.GetWindowRect(rect);
+
+	m_tab.InsertItem(0, _T("메인화면"));
+	/*
+	m_tab.InsertItem(1, _T("게임 1");
+	m_tab.InsertItem(2, _T("게임 2");
+	m_tab.InsertItem(3, _T("점수판");
+	*/
+
+	m_tab.SetCurSel(0);
+	
+	dlg1 = new CDlgTab1;
+	dlg1->Create(IDD_DIALOG_TAB_1_MAIN, &m_tab);
+	dlg1->MoveWindow(0, 20, rect.Width(), rect.Height());
+	dlg1->ShowWindow(SW_SHOW);
+
+	/*
+	dlg2 = new CDlgTab1;
+	dlg2->Create(IDD_DIALOG_TAB_2_GAME, &m_tab);
+	dlg2->MoveWindow(0, 20, rect.Width(), rect.Height());
+	dlg2->ShowWindow(SW_HIDE);
+
+	dlg3 = new CDlgTab1;
+	dlg3->Create(IDD_DIALOG_TAB_3_GAME, &m_tab);
+	dlg3->MoveWindow(0, 20, rect.Width(), rect.Height());
+	dlg3->ShowWindow(SW_HIDE);
+
+	dlg4 = new CDlgTab1;
+	dlg4->Create(IDD_DIALOG_TAB_4_SCORE, &m_tab);
+	dlg4->MoveWindow(0, 20, rect.Width(), rect.Height());
+	dlg4->ShowWindow(SW_HIDE);
+	*/
+	
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CTypingGameDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 애플리케이션의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CTypingGameDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CTypingGameDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CTypingGameDlg::OnSelchangeTab(NMHDR* pNMHDR, LRESULT* pResult)
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	if (IDC_TAB == pNMHDR->idFrom) {
+		int sel = m_tab.GetCurSel();
+		switch (sel) {
+		case 0:
+			dlg1->ShowWindow(SW_SHOW);
+			break;
+
+		}
+	}
+	*pResult = 0;
+}
---
+++ TypingGameDlg.h
@@ -0,0 +1,38 @@
+﻿
+// TypingGameDlg.h: 헤더 파일
+//
+#include "CDlgTab1.h"
+#pragma once
+
+
+// CTypingGameDlg 대화 상자
+class CTypingGameDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CTypingGameDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_TYPINGGAME_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CTabCtrl m_tab;
+	afx_msg void OnSelchangeTab(NMHDR* pNMHDR, LRESULT* pResult);
+	CDlgTab1* dlg1;
+};
